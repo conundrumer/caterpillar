db26725f52ddc94ad9e5a018b32be88affdb6a9e
Noelle Jung <njung1292@gmail.com>
2015-02-07 03:46:39 -0500

    sidebar stuff

--- public/js/src/script.js
+++ public/js/src/script.js
@@ -4,15 +4,23 @@ var tracker = require('./tracker');
 
 function onTrackerSuccess() {
     console.log('got tracking!');
+    makeCaterpillar();
 }
 
 function onTrackerFail(message) {
     console.log('failed to get tracking!', message);
 }
 
+function makeCaterpillar() {
+	canvas = $("#game-canvas")[0];
+	var ctx = canvas.getContext("2d");
+	ctx.font = "30px Arial";
+	ctx.fillText("Nice to meet you.",10,50);
+	$("#go-btn")[0].hidden = false;
+}
+
 window.onload = function() {
     console.log('hey there!!');
-    $("#dummy-webcam").on('click', makeCaterpillar);
     $("#go-btn").on('click', startGame);
     tracker.onUpdate = function(data) {
     	console.log(data);
@@ -24,21 +32,14 @@ store.onChange = function(data) {
     console.log(data[0].id, data[0].position, data[0].heading);
 };
 
-function makeCaterpillar() {
-	canvas = $("#intro-caterpillar")[0];
-	var ctx = canvas.getContext("2d");
-	ctx.font = "30px Arial";
-	ctx.fillText("Nice to meet you.",10,50);
-	$("#go-btn")[0].hidden = false;
-};
-
 function startGame() {
 	// tell server to add the caterpillar
-	data = {};
-	loadGamePage(data);
+	loadGamePage();
 };
 
-function loadGamePage(data) {
-	$(".intro-view").remove();
-	$("#game-view")[0].hidden = false;
+function loadGamePage() {
+	$("#go-btn").remove();
+	canvas = $("#game-canvas")[0];
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
 };
