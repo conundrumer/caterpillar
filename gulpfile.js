f3a93d765b2d08b9306bfe82288bd77fe742f54b
Jing Xiao <notajingoist@gmail.com>
2015-02-07 00:02:15 -0500

    basic styling, changed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,34 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
-// var sass = require('gulp-ruby-sass');
+var sass = require('gulp-ruby-sass');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var watchify = require('watchify');
-// var reactify = require('reactify');
 
 // Compass & Compile our Sass
-// gulp.task('sass', function() {
-//     var path = './public/stylesheets/';
-//     var files = [
-//         path + 'sass/*.scss',
-//         path + 'sass/grid/*.scss'
-//     ]
-//     return gulp.src(files)
-//         .pipe(sass({
-//             compass: true,
-//             // bundleExec: true,
-//             // sourcemap: true,
-//             sourcemapPath: '../sass',
-//             style: 'compressed'
-//         }))
-//         .on('error', function(err) {
-//             console.log(err.message);
-//         })
-//         .pipe(gulp.dest('./public/stylesheets/css/'));
-// });
+gulp.task('sass', function() {
+    var path = './public/stylesheets/';
+    var files = [
+        path + 'sass/*.scss',
+        path + 'sass/grid/*.scss'
+    ]
+    return gulp.src(files)
+        .pipe(sass({
+            compass: true,
+            sourcemapPath: '../sass',
+            style: 'compressed'
+        }))
+        .on('error', function(err) {
+            console.log(err.message);
+        })
+        .pipe(gulp.dest('./public/stylesheets/css/'));
+});
 
 // Watch Files For Changes
-// gulp.task('watch', function() {
-//     gulp.watch('./public/stylesheets/sass/*.scss', ['sass']);
-// });
+gulp.task('watch', function() {
+    gulp.watch('./public/stylesheets/sass/*.scss', ['sass']);
+});
 
 // Browserify
 gulp.task('browserify', function() {
@@ -62,5 +59,4 @@ gulp.task('browserify', function() {
 });
 
 // Default Task
-// gulp.task('default', ['browserify', 'sass', 'watch']);
-gulp.task('default', ['browserify']);
+gulp.task('default', ['browserify', 'sass', 'watch']);
