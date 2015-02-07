fec134cc80282dfa54c50db0b0825cdb6d5c2591
David Lu <davidelu93@gmail.com>
2015-02-07 08:46:08 -0500

    getting closer to acutally rendering

--- public/js/src/view.jsx
+++ public/js/src/view.jsx
@@ -1,13 +1,41 @@
 var React = require('react');
+var _ = require('underscore');
+
+var Player = React.createClass({
+    render: function() {
+        return (
+            <div>{JSON.stringify(this.props)}</div>
+        );
+    }
+})
+
+var Hole = React.createClass({
+    render: function() {
+        return (
+            <div>{JSON.stringify(this.props)}</div>
+        );
+    }
+})
 
 var App = React.createClass({
     render: function() {
         return (
             <div>
                 {
-                    JSON.stringify(this.props.players)
-                } {
-                    JSON.stringify(this.props.holes)
+                    _.pairs(this.props.players).map(function(kv) {
+                        return <Player
+                            key={kv[0]}
+                            heading={kv[1].heading}
+                            position={kv[1].position}
+                            img={kv[1].img}
+                            food={kv[1].food}
+                        />
+                    })
+                }
+                {
+                    this.props.holes.map(function(hole, i) {
+                        return <Hole key={i} position={hole.position} size={hole.size} />
+                    })
                 }
             </div>
         );
